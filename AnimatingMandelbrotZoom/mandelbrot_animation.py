5d7e2d79f75574f8c209f5b45c293f5729253d82
Lukas Kristensen <lukashtxb16@gmail.com>
2023-03-04 23:09:08 +0100

    Notations

--- AnimatingMandelbrotZoom/mandelbrot_animation.py
+++ AnimatingMandelbrotZoom/mandelbrot_animation.py
@@ -21,7 +21,7 @@ def mandelbrot(c):
     z = numpy.zeros_like(c, dtype=complex)
     # z is iteratively updated with the Mandelbrot formula: z = z^2 + c
 
-    divergence_time = numpy.zeros(c.shape, dtype=int)
+    divergence_time = numpy.zeros(c.shape, dtype=numpy.float32)
 
     # Iterate over the complex plane
     for i in range(100):
@@ -44,8 +44,8 @@ def main(x_0, y_0, x_1, y_1):
     start_time = time.time()
 
     # Generates linear spaces with pRE and pIM elements respectively around the plane of the Mandelbrot set
-    x_space = numpy.linspace(x_0, x_1, pRE).reshape((1, pRE))
-    y_space = numpy.linspace(y_0, y_1, pIM).reshape((pIM, 1))
+    x_space = numpy.linspace(x_0, x_1, pRE, dtype=numpy.float32).reshape((1, pRE))
+    y_space = numpy.linspace(y_0, y_1, pIM, dtype=numpy.float32).reshape((pIM, 1))
 
     # Generate a 2D array for each dimension of the complex plane
     complete_space = x_space + y_space * 1j
@@ -70,18 +70,17 @@ if __name__ == '__main__':
         target_y = (interest_points[z][2], interest_points[z][3])
         step_size = interest_points[z][4]
 
-        dif_x0, dif_x1, dif_y0, dif_y1 = target_x[0]-start_x[0], target_x[1]-start_x[1], target_y[0]-start_y[0], target_y[1]-start_y[1]
-        step_x0, step_x1, step_y0, step_y1 = dif_x0/step_size, dif_x1/step_size, dif_y0/step_size, dif_y1/step_size
+        dif_x, dif_y = (target_x[0]-start_x[0], target_x[1]-start_x[1]), (target_y[0]-start_y[0], target_y[1]-start_y[1])
+        step_x, step_y = (dif_x[0]/step_size, dif_x[1]/step_size), (dif_y[0]/step_size, dif_y[1]/step_size)
 
-        print("Differences:", dif_x0, dif_x1, dif_y0, dif_y1)
-        print("Steps:", step_x0, step_x1, step_y0, step_y1)
+        print("Differences:", dif_x, dif_y)
+        print("Steps:", step_x, step_y)
         print("\n\n")
 
         for i in range(step_size+1):
-            print("Applying step calculations:",step_x0*i, step_x1*i, step_y0*i, step_y1*i)
-            current_x = (start_x[0]+step_x0*i, start_x[1]+step_x1*i)
-            current_y = (start_y[0]+step_y0*i, start_y[1]+step_y1*i)
-            # print("\nCurrent x:", current_x, "Current y:", current_y)
+            print("Applying step calculations:",step_x[0]*i, step_x[1]*i, step_y[0]*i, step_y[1]*i)
+            current_x = (start_x[0]+step_x[0]*i, start_x[1]+step_x[1]*i)
+            current_y = (start_y[0]+step_y[0]*i, start_y[1]+step_y[1]*i)
 
             computed_mandelbrot = main(current_x[0], current_y[0], current_x[1], current_y[1])
             plt.imshow(computed_mandelbrot, cmap='magma')
