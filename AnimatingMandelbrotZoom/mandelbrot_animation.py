107b4e8979c2daf936f161eb9fd609f5691b5cfc
Lukas Kristensen <40022921+LukasKristensen@users.noreply.github.com>
2023-03-25 21:02:30 +0100

    Extend float range and info on vid

--- AnimatingMandelbrotZoom/mandelbrot_animation.py
+++ AnimatingMandelbrotZoom/mandelbrot_animation.py
@@ -5,10 +5,10 @@ import cv2
 import os
 import datetime
 
-pRE = 500
-pIM = 500
+pRE = 2000
+pIM = 2000
 threshold = 2
-iterations = 100
+iterations = 500
 
 frames_between_points = 30
 frame_rate = round(frames_between_points/6)
@@ -27,10 +27,10 @@ def mandelbrot(c):
     # Generate a 2D array of ones, which is then converted to a boolean data type array
     mandelbrot_mask = numpy.ones_like(c, dtype=bool)
     # Generate a 2D array of zeros, which is then converted to a complex data type array
-    z = numpy.zeros_like(c, dtype=complex)
+    z = numpy.zeros_like(c, dtype=numpy.complex128)
     # z is iteratively updated with the Mandelbrot formula: z = z^2 + c
 
-    divergence_time = numpy.zeros(c.shape, dtype=numpy.float32)
+    divergence_time = numpy.zeros(c.shape, dtype=numpy.float64)
 
     # Iterate over the complex plane
     for i in range(iterations):
@@ -53,8 +53,8 @@ def main(x_0, y_0, x_1, y_1):
     start_time = time.time()
 
     # Generates linear spaces with pRE and pIM elements respectively around the plane of the Mandelbrot set
-    x_space = numpy.linspace(x_0, x_1, pRE, dtype=numpy.float32).reshape((1, pRE))
-    y_space = numpy.linspace(y_0, y_1, pIM, dtype=numpy.float32).reshape((pIM, 1))
+    x_space = numpy.linspace(x_0, x_1, pRE, dtype=numpy.float64).reshape((1, pRE))
+    y_space = numpy.linspace(y_0, y_1, pIM, dtype=numpy.float64).reshape((pIM, 1))
 
     # Generate a 2D array for each dimension of the complex plane
     complete_space = x_space + y_space * 1j
@@ -100,10 +100,11 @@ if __name__ == '__main__':
 
             # Apply the Mandelbrot formula and generate the image
             complete_mandelbrot = main(current_x[0], current_y[0], current_x[1], current_y[1])
-            plt.figure("output", figsize=(pIM, pRE), dpi=1)
-            plt.imshow(complete_mandelbrot, cmap='magma')
 
-            # Formatting of matplotlib figure removing all axes and padding
+            plt.figure(figsize=(pRE, pIM), dpi=1)
+            plt.imshow(complete_mandelbrot, cmap='magma', interpolation='nearest', aspect='auto')
+            # plt.close()
+            print("Plot size:", plt.gcf().get_size_inches() * plt.gcf().dpi)
             plt.axis('off')
             plt.bbox_inches = 'tight'
             plt.pad_inches = 0
@@ -111,7 +112,11 @@ if __name__ == '__main__':
             plt.margins(0, 0)
             plt.savefig(f'tmp_hold.png', bbox_inches='tight', pad_inches=0)
             plt.close()
-            video_writer.write(cv2.imread('tmp_hold.png'))
+
+            loaded_image = cv2.imread('tmp_hold.png')
+            cv2.putText(loaded_image, f'X: {round(current_x[0], 4)} : {round(current_x[1], 4)}', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+            cv2.putText(loaded_image, f'Y: {round(current_y[0], 4)} : {round(current_y[0], 4)}', (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+            video_writer.write(loaded_image)
 
             # Progress bar and time estimation
             progress_count = round(((i+((z-1)*step_size))/(len(interest_points)-1)/step_size)*100,2)
