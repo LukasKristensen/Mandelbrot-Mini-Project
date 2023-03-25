b36c99e3108b9df43ac2073a400d108265299a71
Lukas Kristensen <40022921+LukasKristensen@users.noreply.github.com>
2023-03-25 20:42:10 +0100

    Update ImageRender Resolution

--- HighResolutionRender/ImageRender.py
+++ HighResolutionRender/ImageRender.py
@@ -4,8 +4,7 @@ import numba
 from matplotlib import pyplot as plt
 import time
 
-pRE = 1000
-pIM = 1000
+map_size = 10000
 threshold = 100
 
 
@@ -21,33 +20,30 @@ def mandelbrot(x, y):
     return 0
 
 
-def main(show_figure=True, save_image=False):
+def main():
     start_time = time.time()
-    solution = [[(0, 0, 0) for x in range(pRE)] for y in range(pIM)]
+    solution = [[(0, 0, 0) for x in range(map_size)] for y in range(map_size)]
 
-    for x in range(pRE):
-        print("Progress: {:.2f}%".format((x/pRE)*100))
-        for y in range(pIM):
-            solution[y][x] = mandelbrot((x-(pRE*0.75))/(pRE*0.35), (y-(pRE*0.5))/(pRE*0.35))
+    for x in range(map_size):
+        print("Progress: {:.2f}%".format((x/map_size)*100))
+        for y in range(map_size):
+            solution[y][x] = mandelbrot((x-(map_size*0.75))/(map_size*0.35), (y-(map_size*0.5))/(map_size*0.35))
 
     print("Computation time:", time.time() - start_time)
 
-    if show_figure and not save_image:
-        plt.imshow(solution, cmap='magma')
-        plt.show()
-    if save_image:
-        plt.figure(figsize=(1000,1000), dpi=1)
-        plt.imshow(solution, cmap='magma')
-        print("Plot size:", plt.gcf().get_size_inches()*plt.gcf().dpi)
-        plt.axis('off')
-        plt.bbox_inches = 'tight'
-        plt.pad_inches = 0
-        plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-        plt.margins(0,0)
-        plt.savefig(f'mandelbrot_numba_{str(datetime.datetime.now()).replace(":","-")}.png')
+    print("Solution size:", len(solution), len(solution[0]))
+    plt.figure(figsize=(map_size, map_size), dpi=1)
+    plt.imshow(solution, cmap='magma', interpolation='nearest', aspect='auto')
+
+    print("Plot size:", plt.gcf().get_size_inches()*plt.gcf().dpi)
+    plt.axis('off')
+    plt.bbox_inches = 'tight'
+    plt.pad_inches = 0
+    plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+    plt.margins(0, 0)
+    plt.savefig(f'mandelbrot_numba_{str(datetime.datetime.now()).replace(":","-")}.png')
 
 
 if __name__ == '__main__':
-    main(save_image=True)
-    # Computation time: 1.39s
+    main()
 
